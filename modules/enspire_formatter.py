456aa47e1da7bc3c25712b450b5de9b4eae196ca
ESimonds910 <esimonds@abscibio.com>
2021-02-25 16:07:35 -0800

    Added Display Ready to Loop

--- modules/enspire_formatter.py
+++ modules/enspire_formatter.py
@@ -5,6 +5,7 @@ class DataFormatter:
 
     def __init__(self):
         self.all_data_signals = pd.DataFrame()
+        self.display_ready_df = pd.DataFrame()
         self.well_ids = ['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09', 'A10', 'A11', 'A12',
                          'B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B09', 'B10', 'B11', 'B12',
                          'C01', 'C02', 'C03', 'C04', 'C05', 'C06', 'C07', 'C08', 'C09', 'C10', 'C11', 'C12',
@@ -17,60 +18,57 @@ class DataFormatter:
 
     def formatter(self, all_enspire_data, all_rep_data, plate_ids):
         for plate in plate_ids:
-            try:
-                main_data_bloc = all_enspire_data[all_enspire_data["Plate"] == plate]
-                rep_data_bloc = all_rep_data[all_rep_data["Plate"] == plate]
-            except KeyError:
-                main_data_bloc = all_enspire_data[all_enspire_data["Plate"] == plate]
-            finally:
-                data_signals_list = []
-                rep_data_signals_list = []
-                for r in range(0, 16, 2):
-                    for c in range(1, 25, 2):
-                        try:
-                            sub_data_bloc = main_data_bloc.iloc[[r, r + 1]][[c, c + 1]]
-                            sub_dna_data_bloc = main_data_bloc.iloc[[r, r + 1]][[c + 24, c + 25]]
-                        except IndexError:
-                            sub_data_bloc = rep_data_bloc.iloc[[r, r + 1]][[c, c + 1]]
-                            sub_dna_data_bloc = rep_data_bloc.iloc[[r, r + 1]][[c + 24, c + 25]]
-
-                            new_list = list(sub_data_bloc.iloc[0]) + \
-                                       list(sub_data_bloc.iloc[1]) + \
-                                       list(sub_dna_data_bloc.iloc[0]) + \
-                                       list(sub_dna_data_bloc.iloc[1])
-                            rep_data_signals_list.append(new_list)
-                        else:
-                            new_list = list(sub_data_bloc.iloc[0]) + \
-                                       list(sub_data_bloc.iloc[1]) + \
-                                       list(sub_dna_data_bloc.iloc[0]) + \
-                                       list(sub_dna_data_bloc.iloc[1])
-                            data_signals_list.append(new_list)
-
-                if data_signals_list:
-                    data_signals_df = pd.DataFrame(
-                        data_signals_list,
-                        index=[x + 1 for x in range(len(data_signals_list))],
-                        columns="Alpha_1 Alpha_2 Alpha_3 Alpha_4 DNA_1 DNA_2 DNA_3 DNA_4".split()
-                    )
-                    data_signals_df.insert(0, "Well ID", self.well_ids, True)
-                    data_signals_df.insert(1, "Source", plate[:2], True)
-                    data_signals_df.insert(2, "Plate", plate, True)
-                    data_signals_df.insert(0, "ID", data_signals_df["Source"] + "-" + data_signals_df["Well ID"], True)
-                    data_signals_df.set_index("ID", inplace=True)
-                    self.all_data_signals = pd.concat([self.all_data_signals, data_signals_df])
-                    # pd.DataFrame.to_csv(self.all_data_signals, "all_data_df.csv")
-                else:
-                    rep_data_signals_df = pd.DataFrame(
-                        rep_data_signals_list,
-                        index=[x + 1 for x in range(len(rep_data_signals_list))],
-                        columns="Alpha_1 Alpha_2 Alpha_3 Alpha_4 DNA_1 DNA_2 DNA_3 DNA_4".split()
-                    )
-
-                    rep_data_signals_df.insert(0, "Well ID", self.well_ids, True)
-                    rep_data_signals_df.insert(1, "Source", plate[:2], True)
-                    rep_data_signals_df.insert(2, "Plate", plate, True)
-                    rep_data_signals_df.insert(0, "ID", rep_data_signals_df["Source"] +
-                                               "-" + rep_data_signals_df["Well ID"], True)
+            if len(plate) == 2 or plate.split("-")[1] == "1":
+                wl = 0
+                end_row = 16
+                for row in range(0, end_row, 2):
+                    end_col = int(all_enspire_data.shape[1] / 2)
+                    for col in range(1, end_col, 2):
+                        dna_col_id = col + 24
+                        bloc_df = pd.DataFrame([[plate] + [self.well_ids[wl]] +
+                                                list(all_enspire_data.iloc[row][[col, col + 1]])
+                                                + list(all_enspire_data.iloc[row + 1][[col, col + 1]]) +
+                                                list(all_enspire_data.iloc[row][[dna_col_id, dna_col_id + 1]])
+                                                + list(all_enspire_data.iloc[row + 1][[dna_col_id, dna_col_id + 1]])])
+                        display_bloc = pd.DataFrame([[self.well_ids[wl] for n in range(4)],
+                                                         list(bloc_df.iloc[0, 2:6]),
+                                                         list(bloc_df.iloc[0, 6:])],
+                                                        index="Well_Id Alpha DNA".split()).transpose()
+                        display_bloc.insert(1, "plate", plate)
+                        display_bloc.insert(2, "row", display_bloc["Well_Id"].apply(lambda x: x[:1]))
+                        display_bloc.insert(3, "col", display_bloc["Well_Id"].apply(lambda x: x[2:]))
+                        wl += 1
+                        self.all_data_signals = pd.concat([self.all_data_signals, bloc_df])
+                        self.display_ready_df = pd.concat([self.display_ready_df, display_bloc])
+                print(self.all_data_signals)
+                print(self.display_ready_df)
+            # if data_signals_list:
+            #     data_signals_df = pd.DataFrame(
+            #         data_signals_list,
+            #         index=[x + 1 for x in range(len(data_signals_list))],
+            #         columns="Alpha_1 Alpha_2 Alpha_3 Alpha_4 DNA_1 DNA_2 DNA_3 DNA_4".split()
+            #     )
+            #
+            #     pd.DataFrame.to_csv(display_bloc, "Display_Ready.csv")
+            #     data_signals_df.insert(0, "Well ID", self.well_ids, True)
+            #     data_signals_df.insert(1, "Source", plate[:2], True)
+            #     data_signals_df.insert(2, "Plate", plate, True)
+            #     data_signals_df.insert(0, "ID", data_signals_df["Source"] + "-" + data_signals_df["Well ID"], True)
+            #     data_signals_df.set_index("ID", inplace=True)
+            #     self.all_data_signals = pd.concat([self.all_data_signals, data_signals_df])
+            #     # pd.DataFrame.to_csv(self.all_data_signals, "all_data_df.csv")
+            # else:
+            #     rep_data_signals_df = pd.DataFrame(
+            #         rep_data_signals_list,
+            #         index=[x + 1 for x in range(len(rep_data_signals_list))],
+            #         columns="Alpha_1 Alpha_2 Alpha_3 Alpha_4 DNA_1 DNA_2 DNA_3 DNA_4".split()
+            #     )
+            #
+            #     rep_data_signals_df.insert(0, "Well ID", self.well_ids, True)
+            #     rep_data_signals_df.insert(1, "Source", plate[:2], True)
+            #     rep_data_signals_df.insert(2, "Plate", plate, True)
+            #     rep_data_signals_df.insert(0, "ID", rep_data_signals_df["Source"] +
+            #                                "-" + rep_data_signals_df["Well ID"], True)
 
                 # TODO Need to create rep dataframes
 
